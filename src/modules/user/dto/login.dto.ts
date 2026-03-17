5a96e0678ca571173387c61b4a1f01a2f3f0bc87
tkuku <taras.kukuliak@aravo.com>
2026-03-17 13:09:39 +0200

    feat: Complete basic auth in user module

--- src/modules/user/dto/login.dto.ts
+++ src/modules/user/dto/login.dto.ts
@@ -1,11 +1,9 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
-  @IsString()
   @IsNotEmpty()
   name: string;
 
-  @IsString()
   @IsNotEmpty()
   password: string;
 }
