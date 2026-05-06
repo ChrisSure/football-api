bda50aab9df4fff7b0550ed84d33521648830abc
tkuku <taras.kukuliak@aravo.com>
2026-05-06 10:51:24 +0300

    improve: Db connection

--- src/core/db/config/typeorm.config.ts
+++ src/core/db/config/typeorm.config.ts
@@ -5,12 +5,12 @@ config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '51.89.14.91',
   port: parseInt(process.env.DB_PORT || '3306'),
-  username: process.env.DB_USERNAME || 'root',
+  username: process.env.DB_USERNAME || 'teddy',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_DATABASE || 'xfootball',
-  entities: ['src/modules/**/*.entity.ts'],
+    entities: ['dist/**/*.entity.js'],
   migrations: ['src/core/db/migrations/*.ts'],
   synchronize: false,
   logging: process.env.DB_LOGGING === 'true',
